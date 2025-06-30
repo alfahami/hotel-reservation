e8ba7e08630f1f228200a3f4e44614452d1f3bdd
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-06-30 19:43:22 +0100

    chores: initialize project with maven

---
+++ src/test/java/com/codelogium/booking/AppTest.java
@@ -0,0 +1,20 @@
+package com.codelogium.booking;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

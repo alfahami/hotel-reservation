60ba5862dba842a8f001ea800c41d02c0cb8ceba
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-06-30 20:21:39 +0100

    feat: implemnt basic crud for repos

--- src/main/java/com/codelogium/booking/repository/RoomRepository.java
+++ src/main/java/com/codelogium/booking/repository/RoomRepository.java
@@ -1,5 +1,29 @@
 package com.codelogium.booking.repository;
 
+import java.util.ArrayList;
+
+import com.codelogium.booking.entity.Room;
+
 public class RoomRepository {
     
+    private ArrayLis<Room> datastore = new ArrayList<>();
+
+    public Boolean createRoom(Room room) {
+        return this.datastore.add(room);
+    }
+
+    public retrieveRoom(String id) {
+        return findRoom(id);
+    }
+
+    public Boolean removeRoom(String id) {
+        return datastore.reomve(findRoom(id));
+    }
+
+    private Room findRoom(String id) {
+        for (Room room : datastore) {
+            if(room.getId().equals(id)) return new Room(room); // use deep copy to protect the integrity of the data stored in our arrayList
+        }
+        return null; // TODO: custom Exception
+    }
 }
--- src/main/java/com/codelogium/booking/repository/UserRepository.java
+++ src/main/java/com/codelogium/booking/repository/UserRepository.java
@@ -1,5 +1,26 @@
 package com.codelogium.booking.repository;
 
+import java.util.ArrayList;
+
+import com.codelogium.booking.entity.User;
+
 public class UserRepository {
     
+    private ArrayList<User> datastore = new ArrayList<>();
+
+    public Boolean createUser(User user) {
+        return this.datastore.add(user);
+    }
+
+    public User retrieveUser(String id) {
+
+    }
+
+    private User findUser(String id) {
+        for (User user : datastore) {
+            if(user.getId().equals(id)) return new User(user);
+        }
+
+        return null; // TODO: custom Exception
+    }
 }

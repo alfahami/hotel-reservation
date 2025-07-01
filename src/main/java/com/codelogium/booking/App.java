17fadb24d703a1d9ffbe149c11b28006b5406372
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-07-01 20:09:27 +0100

    test: booking creation

--- src/main/java/com/codelogium/booking/App.java
+++ src/main/java/com/codelogium/booking/App.java
@@ -1,8 +1,9 @@
 package com.codelogium.booking;
 
-import java.util.Scanner;
+import java.time.LocalDate;
 
 import com.codelogium.booking.constants.RoomType;
+import com.codelogium.booking.entity.User;
 import com.codelogium.booking.repository.BookingRepository;
 import com.codelogium.booking.repository.RoomRepository;
 import com.codelogium.booking.repository.UserRepository;
@@ -27,6 +28,19 @@ public class App
          bookingService.setRoom(1, RoomType.STANDARD, 1000);
          bookingService.setRoom(2, RoomType.JUNIOR, 2000);
          bookingService.setRoom(3, RoomType.SUITE, 3000);
+
+         // Create 3 user
+         User user1 = new User(1, "John Doe", "NBE111", 5000);
+         User user2 = new User(2, "Jane Doe", "NBE222", 10000);
+         User user3 = new User(3, "Smith Sailor", "NBE444", 50000);
+
+         userRepository.createUser(user1);
+         userRepository.createUser(user2);
+         userRepository.createUser(user3);
+
+         // Booking
+         bookingService.bookRoom(1, 2, LocalDate.parse("2026-06-30"), LocalDate.parse("2026-07-07"));
+
     }
 
     

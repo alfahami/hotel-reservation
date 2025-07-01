808f332bfbb80ac0826c43e443e0f1b302ae904e
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-07-01 10:23:18 +0100

    feat: generate business service methods

--- src/main/java/com/codelogium/booking/service/BookingServiceImpl.java
+++ src/main/java/com/codelogium/booking/service/BookingServiceImpl.java
@@ -1,5 +1,8 @@
 package com.codelogium.booking.service;
 
+import java.time.LocalDate;
+
+import com.codelogium.booking.constants.RoomType;
 import com.codelogium.booking.repository.RoomRepository;
 import com.codelogium.booking.repository.UserRepository;
 
@@ -7,5 +10,30 @@ public class BookingServiceImpl implements BookingService {
     
     private UserRepository userRepository;
     private RoomRepository roomRepository;
-
+    @Override
+    public void setRoom(int roomNumber, RoomType roomType, int rate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setRoom'");
+    }
+    @Override
+    public void bookRoom(int userId, int roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bookRoom'");
+    }
+    @Override
+    public void printAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printAll'");
+    }
+    @Override
+    public void setUser(int userId, int balance) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setUser'");
+    }
+    @Override
+    public void printAllUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printAllUser'");
+    }
+    
 }

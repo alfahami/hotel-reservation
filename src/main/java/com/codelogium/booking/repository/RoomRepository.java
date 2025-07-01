80be374ec678c8831f05608a1c8d2204cad4fba4
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-07-01 16:37:40 +0100

    feat: implement update room service

--- src/main/java/com/codelogium/booking/repository/RoomRepository.java
+++ src/main/java/com/codelogium/booking/repository/RoomRepository.java
@@ -13,17 +13,31 @@ public class RoomRepository {
     }
 
     public Room retrieveRoom(String id) {
-        return findRoom(id);
+        return findRoomById(id);
+    }
+
+    // Update the room based on its room number
+    public Room updateRoom(int roomNumber, Room updatedRoom) {
+        Room room = findRoomByNumber(roomNumber);
+       return  datastore.set(datastore.indexOf(room), updatedRoom); 
     }
 
     public Boolean removeRoom(String id) {
-        return datastore.remove(findRoom(id));
+        return datastore.remove(findRoomById(id));
     }
 
-    private Room findRoom(String id) {
+    private Room findRoomById(String id) {
         for (Room room : datastore) {
             if(room.getId().equals(id)) return new Room(room); // use deep copy to protect the integrity of the data stored in our arrayList
         }
         return null; // TODO: custom Exception
+    }
+
+    public Room findRoomByNumber(int roomNumber) {
+        for (Room room : datastore) {
+            if(room.getRoomNumber() == roomNumber) return new Room(room);
+        }
+
+        return null; // TODO: custom exception
     }
 }

216562fcbb78806754e7c8b63c23c07a5d25f026
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-07-01 17:19:57 +0100

    test: room creation and set up

--- src/main/java/com/codelogium/booking/App.java
+++ src/main/java/com/codelogium/booking/App.java
@@ -1,9 +1,33 @@
 package com.codelogium.booking;
 
+import java.util.Scanner;
+
+import com.codelogium.booking.constants.RoomType;
+import com.codelogium.booking.repository.BookingRepository;
+import com.codelogium.booking.repository.RoomRepository;
+import com.codelogium.booking.repository.UserRepository;
+import com.codelogium.booking.service.BookingService;
+import com.codelogium.booking.service.BookingServiceImpl;
+
 public class App 
 {
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        // Setting up our ArrayList
+         RoomRepository roomRepository = new RoomRepository();
+         UserRepository userRepository = new UserRepository();
+         BookingRepository bookingRepository = new BookingRepository();
+
+
+         BookingService bookingService = new BookingServiceImpl(userRepository, roomRepository, bookingRepository);
+
+         // Create 3 rooms
+         bookingService.setRoom(1, RoomType.STANDARD, 1000);
+         bookingService.setRoom(2, RoomType.JUNIOR, 2000);
+         bookingService.setRoom(3, RoomType.SUITE, 3000);
     }
+
+    
 }
